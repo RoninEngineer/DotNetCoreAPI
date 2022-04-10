21ae81f9e0d8ac738361f525c1b7023aeaaa151e
Jim Walsh <walshjrjames@outlook.com>
2022-04-09 20:06:33 -0400

    Added some table context work

--- DotNetCoreAPI.Data/Resource/TableStorageContext.cs
+++ DotNetCoreAPI.Data/Resource/TableStorageContext.cs
@@ -1,7 +1,10 @@
-﻿using Azure.Data.Tables;
+﻿using Azure;
+using Azure.Data.Tables;
 using DotNetCoreAPI.Data.Interface;
+using DotNetCoreAPI.Domain.Dto.Tables;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DotNetCoreAPI.Data.Resource
@@ -13,5 +16,7 @@ namespace DotNetCoreAPI.Data.Resource
         {
             _tableClient = tableClient;
         }
+
+        
     }
 }
---
+++ DotNetCoreAPI.Domain/Dto/Tables/IntegrationData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCoreAPI.Domain.Dto.Tables
+{
+    public class IntegrationData : TableEntity
+    {
+
+		public IntegrationData(string description, string functionInvocationId)
+		{
+			this.PartitionKey = description;
+			this.RowKey = functionInvocationId;
+		}
+
+		public bool? Active { get; set; }
+
+		public string Type { get; set; }
+
+		public string Description { get; set; }
+
+		public int? SequenceId { get; set; }
+
+	}
+}
